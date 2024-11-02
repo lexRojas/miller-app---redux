ee67b1ccece4a1d8e95e61333851a48ee06d5653
Rodrigo Rojas Chaves <66186351+lexRojas@users.noreply.github.com>
2024-11-01 23:19:11 -0600

    eeee

--- src/tools/converPDF.js
+++ src/tools/converPDF.js
@@ -17,7 +17,7 @@ export const converPDF = (component) => {
 
     //   pdf.addImage(imgData, "PNG", xOffset, yOffset, imgWidth, imgHeight);  
       pdf.addImage(imgData, "PNG", xOffset, yOffset);  
-      pdf.save("miller-boleta.pdf");
+      pdf.save("millerboleta.pdf");
     });
   };
   
